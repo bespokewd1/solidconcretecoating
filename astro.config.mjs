e15a14031fabbde24a8694fa3022f6dc6b43db1b
churchilldotexe <churchillexe@gmail.com>
2025-12-13 02:52:46 +0800

    chore: optimization for links to prefetch pages, auto play on restoration service

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://www.yourwebsite.com", // update me!
+prefetch: true,
   integrations: [
     icon(),
     sitemap({
